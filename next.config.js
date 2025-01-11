536c3bbf63690fb522d611776497b4afbd78363d
enkhbold470 <enkhbold470@gmail.com>
2025-01-11 15:49:24 -0800

    Add accent route and update site name
    - Added a new route for the accent page.
    - Updated the site name in the site configuration file.
    
    Fix input component styling
    
    - Fixed the styling of the input component to ensure consistent appearance.
    
    Add login page and related components
    
    - Added a new login page with a login form component.
    - Created a label component for form inputs.
    
    Configure Next.js with PWA support
    
    - Configured Next.js to support progressive web app (PWA) features.
    - Added runtime caching for improved performance.
    
    Redirect home page to login page
    
    - Updated the home page to redirect to the login page.
    - Added a temporary message for the redirection.
    
    Add card component and related components
    
    - Added a new card component with header, title, description, content, and footer sections.
    - Created separate components for each section of the card.
    
    Update Next.js configuration
    
    - Updated the Next.js configuration file with additional settings.
    - Enabled SWC minification and optimized image loading.

--- next.config.js
+++ next.config.js
@@ -1,4 +1,35 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+import withPWA from "next-pwa"
+import runtimeCaching from "next-pwa/cache.js"
 
-module.exports = nextConfig
+const pwaConfig = withPWA({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  runtimeCaching,
+})
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: false,
+  ...pwaConfig,
+  swcMinify: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.pexels.com",
+      },
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "www.pexels.com",
+      },
+    ],
+  },
+}
+// module.exports = withPWA(nextConfig);
+
+export default nextConfig
